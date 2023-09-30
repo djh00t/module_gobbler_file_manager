6a29c5c29e9eeec6d0893aa7b7d1cceefd754d2f
David Hooton <david@hooton.org>
2023-09-30 16:38:10 +1000

    Set s3 bucket name, create and upload test files, assert file content.

--- tests/test_gobbler_file_manager.py
+++ tests/test_gobbler_file_manager.py
@@ -22,23 +22,30 @@ aws_secret_access_key = aws_credentials['credentials']['AWS_SECRET_ACCESS_KEY']
 # from the credentials object.
 assert aws_access_key_id is not None
 assert aws_secret_access_key is not None
+# Set s3 bucket name
+s3_bucket_name = 'fsg-gobbler'
 
 # Generate a 100 word string of lorem ipsum text
 #test_txt_content = lorem.text()
 test_txt_content = "Hello World!"
+test_bin_content = b'\x00\x00\x00\x00\x00\x00\x00\x00'
 
 # Test Files - get
-test_txt_get_local = 'tests/test_get_txt_file.txt'
-test_bin_get_local = 'tests/test_get_bin_file.wav'
-test_txt_get_s3 = 's3://fsg-gobbler/fsg-test/test_get_txt_file.txt'
-test_bin_get_s3 = 's3://fsg-gobbler/fsg-test/test_get_bin_file.wav'
+test_txt_get = 'tests/test_get_txt_file.txt'
+test_bin_get = 'tests/test_get_bin_file.wav'
 
 # Test Files - post
-test_txt_post_local = 'tests/test_post_txt_file.txt'
-test_bin_post_local = 'tests/test_post_bin_file.wav'
-test_txt_post_s3 = 's3://fsg-gobbler/fsg-test/test_post_txt_file.txt'
-test_bin_post_s3 = 's3://fsg-gobbler/fsg-test/test_post_bin_file.wav'
+test_txt_post = 'tests/test_post_txt_file.txt'
+test_bin_post = 'tests/test_post_bin_file.wav'
 
+
+# Make sure that a file exists at a given s3 path and contains the expected
+# content.
+def assert_s3_file_content(path, content):
+    s3 = boto3.resource('s3')
+    obj = s3.Object(s3_bucket_name, path)
+    assert obj.content_length > 0
+    assert obj.get()['Body'].read().decode() == content
 
 # Create test files
 def setup_test_files():
@@ -46,90 +53,118 @@ def setup_test_files():
     ## GET TEST FILES
     ##
     # Create local test txt file
-    with open(test_txt_get_local, 'w') as f:
+    with open(test_txt_get, 'w') as f:
         f.write(test_txt_content)
-    # Make sure that the test_txt_get_local file was created
-    assert os.path.exists(test_txt_get_local)
+    # Make sure that the test_txt_get file was created
+    assert os.path.exists(test_txt_get)
+    
     # Create local test binary file
-    with open(test_bin_get_local, 'wb') as f:
-        f.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
-    # Make sure that the test_bin_get_local file was created
-    assert os.path.exists(test_bin_get_local)
-    # Upload test_txt_get_local file to test_txt_get_s3 location
+    with open(test_bin_get, 'wb') as f:
+        f.write(test_bin_content)
+    # Make sure that the test_bin_get file was created
+    assert os.path.exists(test_bin_get)
+
+    # Upload test_txt_get file to test_txt_get location
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_txt_get_local, 'fsg-gobbler', 'fsg-test/test_get_txt_file.txt')
-    # Make sure that the test_txt_get_s3 file was created
-    assert s3.Object('fsg-gobbler', 'fsg-test/test_get_txt_file.txt').content_length > 0
-    # Upload test_bin_get_local file to test_bin_get_s3 location
+    s3.meta.client.upload_file(test_txt_get, s3_bucket_name, test_txt_get)
+    # Make sure that the test_txt_get file was created
+    assert assert_s3_file_content(test_txt_get, test_txt_content)
+    
+    # Upload test_bin_get file to test_bin_get_s3 location
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_bin_get_local, 'fsg-gobbler', 'fsg-test/test_get_bin_file.wav')
+    s3.meta.client.upload_file(test_bin_get, s3_bucket_name, test_bin_get)
     # Make sure that the test_bin_get_s3 file was created
-    assert s3.Object('fsg-gobbler', 'fsg-test/test_get_bin_file.wav').content_length > 0
+    assert assert_s3_file_content(test_bin_get, test_bin_content)
 
     ##
     ## POST TEST FILES
     ##
     # Create local test txt file
-    with open(test_txt_post_local, 'w') as f:
+    with open(test_txt_post, 'w') as f:
         f.write(test_txt_content)
     # Create local test binary file
-    with open(test_bin_post_local, 'wb') as f:
-        f.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
-    # Upload test_txt_post_local file to test_txt_post_s3 location
+    with open(test_bin_post, 'wb') as f:
+        f.write(test_bin_content)
+    # Upload test_txt_post file to test_txt_post_s3 location
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_txt_post_local, 'fsg-gobbler', 'fsg-test/test_post_txt_file.txt')
-    # Upload test_bin_post_local file to test_bin_post_s3 location
+    s3.meta.client.upload_file( test_txt_post, s3_bucket_name, test_txt_post )
+    # Make sure that the test_txt_post_s3 file was created
+    assert assert_s3_file_content( "s3://"+s3_bucket_name+"/"+test_txt_post, test_txt_content )
+    # Upload test_bin_post file to test_bin_post_s3 location
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_bin_post_local, 'fsg-gobbler', 'fsg-test/test_post_bin_file.wav')
+    s3.meta.client.upload_file(test_bin_post, s3_bucket_name, test_bin_post )
 
 def teardown_test_files():
     ##
     ## GET TEST FILES
     ##
     # Delete local test txt file
-    os.remove(test_txt_get_local)
+    os.remove(test_txt_get)
     # Delete local test binary file
-    os.remove(test_bin_get_local)
-    # Delete test_txt_get_s3 file
+    os.remove(test_bin_get)
+    # Delete test_txt_get file
     s3 = boto3.resource('s3')
-    s3.Object('fsg-gobbler', 'fsg-test/test_get_txt_file.txt').delete()
+    s3.Object(s3_bucket_name, test_txt_get).delete()
     # Delete test_bin_get_s3 file
     s3 = boto3.resource('s3')
-    s3.Object('fsg-gobbler', 'fsg-test/test_get_bin_file.wav').delete()
+    s3.Object(s3_bucket_name, test_bin_get).delete()
 
     ##
     ## POST TEST FILES
     ##
     # Delete local test txt file
-    os.remove(test_txt_post_local)
+    os.remove(test_txt_post)
     # Delete local test binary file
-    os.remove(test_bin_post_local)
+    os.remove(test_bin_post)
     # Delete test_txt_post_s3 file
     s3 = boto3.resource('s3')
-    s3.Object('fsg-gobbler', 'fsg-test/test_post_txt_file.txt').delete()
+    s3.Object(s3_bucket_name, 'fsg-test/test_post_txt_file.txt').delete()
     # Delete test_bin_post_s3 file
     s3 = boto3.resource('s3')
-    s3.Object('fsg-gobbler', 'fsg-test/test_post_bin_file.wav').delete()
+    s3.Object(s3_bucket_name, 'fsg-test/test_post_bin_file.wav').delete()
+
 
 # Setup test files
 setup_test_files()
 
 # Test 1 - get local text file
 def test_get_local_txt_file():
-    result = manage_file('get', None, test_txt_get_local, False, True)
+    result = manage_file('get', None, test_txt_get, False, True)
     assert result['status'] == 200
     assert result['action'] == 'get'
     assert result['file'].decode() == test_txt_content
-    assert result['path'] == test_txt_get_local
+    assert result['path'] == test_txt_get
     assert result['binary'] == False
-    assert 'debug' in result
-    assert isinstance(result['debug'], dict)
+    #assert 'debug' in result
+    #assert isinstance(result['debug'], dict)
 
+# Test 2 - get local binary file
+def test_get_local_bin_file():
+    result = manage_file('get', None, test_bin_get, False, True)
+    assert result['status'] == 200
+    assert result['action'] == 'get'
+    assert result['file'] == b'\x00\x00\x00\x00\x00\x00\x00\x00'
+    assert result['path'] == test_bin_get
+    assert result['binary'] == True
+    #assert 'debug' in result
+    #assert isinstance(result['debug'], dict)
 
+# Test 3 - get s3 text file
+def test_get_s3_txt_file():
+    result = manage_file('get', None, test_txt_get, False, True)
+    #print(result)
+    assert result['status'] == 200
+    assert result['action'] == 'get'
+    assert result['file'].decode() == test_txt_content
+    assert result['path'] == test_txt_get
+    assert result['binary'] == False
+    #assert 'debug' in result
+    #assert isinstance(result['debug'], dict)
 
+#test_get_s3_txt_file()
 
 # Sleep for 5 seconds
-time.sleep(5)
+#time.sleep(5)
 
 # Cleanup test files
 #teardown_test_files()
