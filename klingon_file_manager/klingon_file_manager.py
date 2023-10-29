e720ff589c3969dee835bec6b656ccdddc8d09fb
David Hooton <david@hooton.org>
2023-10-30 10:35:25 +1100

    aider: Fix manage_file function to accept metadata argument and pass it to write_file function.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     python test.py
    
    And got this output:
    
    100+0 records in
    100+0 records out
    104857600 bytes transferred in 0.023027 secs (4553680462 bytes/sec)
    md (binary):                    b'/(+\x84\xe7\xe6\x08\xd5\x85$I\xed\x94\x0b\xfcQ'
    contents_md5 (base64):          LygrhOfmCNWFJEntlAv8UQ==
    The MD5 Hash of this file is:   2f282b84e7e608d5852449ed940bfc51
    The file size is: 104857600 bytes
    Traceback (most recent call last):
      File "/Users/djh/work/src/github.com/djh00t/module_klingon_file_manager/test.py", line 209, in <module>
        test_large_upload_progress()
      File "/Users/djh/work/src/github.com/djh00t/module_klingon_file_manager/test.py", line 196, in test_large_upload_progress
        manage_file(
    TypeError: manage_file() got an unexpected keyword argument 'metadata'
    
    ASSISTANT: The error message indicates that the `manage_file` function does not accept an argument named `metadata`. To fix this, we need to modify the `manage_file` function in the `klingon_file_manager.py` file to accept a `metadata` argument and pass it to the `write_file` function.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    ```python
    klingon_file_manager/klingon_file_manager.py
    <<<<<<< SEARCH
    def manage_file(
        action: str,
        path: str,
        content: str = None,
        md5: Optional[str] = None,
        debug: bool = False,
    ) -> dict:
    =======
    def manage_file(
        action: str,
        path: str,
        content: str = None,
        md5: Optional[str] = None,
        metadata: Optional[Dict] = None,
        debug: bool = False,
    ) -> dict:
    >>>>>>> REPLACE
    ```
    
    ```python
    klingon_file_manager/klingon_file_manager.py
    <<<<<<< SEARCH
    elif action == 'post':
        write_result = write_file(path, content, md5, debug)
    =======
    elif action == 'post':
        write_result = write_file(path, content, md5, metadata, debug)
    >>>>>>> REPLACE
    ```
    
    Please apply these changes to the `klingon_file_manager.py` file.

--- klingon_file_manager/klingon_file_manager.py
+++ klingon_file_manager/klingon_file_manager.py
@@ -40,6 +40,7 @@ def manage_file(
     path: str,
     content: str = None,
     md5: Optional[str] = None,
+    metadata: Optional[Dict] = None,
     debug: bool = False,
 ) -> dict:
 
@@ -80,7 +81,7 @@ def manage_file(
             if debug or result['status'] == 500:
                 debug_info['read_file'] = read_result['debug']
         elif action == 'post':
-            write_result = write_file(path, content, md5, debug)
+            write_result = write_file(path, content, md5, metadata, debug)
             result['status'] = write_result['status']
             # Calculate the size in megabytes rounded to 6 decimal places of
             # the file object
