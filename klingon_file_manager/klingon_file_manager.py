b0672cc1138a046900ff95406fb12d9d710d11d9
David Hooton <david@hooton.org>
2023-10-31 00:22:51 +1100

    Refactor debug_info['write_file_start'] to include only the first 10 characters of the content.

--- klingon_file_manager/klingon_file_manager.py
+++ klingon_file_manager/klingon_file_manager.py
@@ -66,7 +66,7 @@ def manage_file(
             if debug or result['status'] == 500:
                 debug_info['read_file'] = read_result['debug']
         elif action == 'post':
-            debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content}, md5={md5}, metadata={metadata}"
+            debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content[:10]}, md5={md5}, metadata={metadata}"
             write_result = write_file(path, content, md5, metadata, debug)
             result['status'] = write_result['status']
             result['binary'] = is_binary_file(result['content'], debug)
