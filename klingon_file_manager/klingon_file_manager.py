4bdf8cd2feaa065b56e102c3b870d4ffaa3b5352
David Hooton <david@hooton.org>
2023-10-30 23:28:58 +1100

    aider: Fix issue with `manage_file` function not returning a value when the action is 'post'.
    # Aider chat conversation:
    
    ASSISTANT: The `None` result indicates that the `manage_file` function in `klingon_file_manager.py` is not returning any value when the action is 'post'. This is likely because the `manage_file` function does not have a return statement in the 'post' action block.
    
    To fix this, we need to add a return statement to the 'post' action block in the `manage_file` function in `klingon_file_manager.py`.
    
    Here is the *SEARCH/REPLACE block* for the necessary change:
    
    ```python
    klingon_file_manager/klingon_file_manager.py
    <<<<<<< SEARCH
            elif action == 'post':
                debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content}, md5={md5}, metadata={metadata}, Callback={Callback}"
                write_result = write_file(path, content, md5, metadata, debug, Callback)
                result['status'] = write_result['status']
                result['binary'] = is_binary_file(result['content'], debug)
                # Add the debug info for the write_file() function
                if debug or result['status'] == 500:
                    debug_info['write_file'] = write_result['debug']
    =======
            elif action == 'post':
                debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content}, md5={md5}, metadata={metadata}"
                write_result = write_file(path, content, md5, metadata, debug)
                result['status'] = write_result['status']
                result['binary'] = is_binary_file(result['content'], debug)
                # Add the debug info for the write_file() function
                if debug or result['status'] == 500:
                    debug_info['write_file'] = write_result['debug']
                return result
    >>>>>>> REPLACE
    ```
    
    This change adds a return statement to the 'post' action block in the `manage_file` function in `klingon_file_manager.py`. After this change, the `manage_file` function should return a result when the action is 'post', and the file upload should work as expected.

--- klingon_file_manager/klingon_file_manager.py
+++ klingon_file_manager/klingon_file_manager.py
@@ -66,13 +66,14 @@ def manage_file(
             if debug or result['status'] == 500:
                 debug_info['read_file'] = read_result['debug']
         elif action == 'post':
-            debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content}, md5={md5}, metadata={metadata}, Callback={Callback}"
-            write_result = write_file(path, content, md5, metadata, debug, Callback)
+            debug_info['write_file_start'] = f"Starting write_file with path={path}, content={content}, md5={md5}, metadata={metadata}"
+            write_result = write_file(path, content, md5, metadata, debug)
             result['status'] = write_result['status']
             result['binary'] = is_binary_file(result['content'], debug)
             # Add the debug info for the write_file() function
             if debug or result['status'] == 500:
                 debug_info['write_file'] = write_result['debug']
+            return result
             return result
         elif action == 'delete':
             delete_result = delete_file(path, debug)
