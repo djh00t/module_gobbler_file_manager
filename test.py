c0e7a3bd278016eb554faf3f6e4267729865fdb1
David Hooton <david@hooton.org>
2023-10-30 17:54:47 +1100

    Removed unused imports, deleted unused functions, and cleaned up code.

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from klingon_file_manager import manage_file
 from klingon_file_manager.utils import ProgressPercentage
 import base64
-import boto3
 import hashlib
 import lorem
 import os
@@ -30,140 +29,12 @@ test_txt_post = 'tests/test_post_txt_file.txt'
 test_bin_post = 'tests/test_post_bin_file.wav'
 
 
-def compare_s3_local_file(local_file, s3_file):
-    # Download the S3 file to a tmp file name
-    s3 = boto3.resource('s3')
-    s3.meta.client.download_file(s3_bucket_name, s3_file, 'tests/tmp')
-    # Make sure that the tmp file was created
-    assert os.path.exists('tests/tmp')
-    # Compare the local file to the tmp file
-    local_file_content = open(local_file, 'rb').read()
-    tmp_file_content = open('tests/tmp', 'rb').read()
-    # Make sure that the local file and the tmp file have the same content
-    assert local_file_content == tmp_file_content
-    # Delete the tmp file
-    os.remove('tests/tmp')
-
-# Cleanup test files
-def cleanup_test_files():
-    ##
-    ## Remove any test files created by other test runs
-    ##
-    # Create array of test file URLs and paths
-    test_files = [
-        "s3://"+s3_bucket_name+"/"+test_txt_post,
-        "s3://"+s3_bucket_name+"/"+test_bin_post,
-        "s3://"+s3_bucket_name+"/"+test_txt_get,
-        "s3://"+s3_bucket_name+"/"+test_bin_get,
-        test_txt_post,
-        test_bin_post,
-        test_txt_get,
-        test_bin_get
-    ]
-    # Loop through the test files and delete them if they exist and log if they
-    # existed or not, and if they were deleted or not
-    for test_file in test_files:
-        # Check if the test file exists
-        if manage_file('get', test_file, None)['status'] == 200:
-            # If the test file exists, delete it
-            result = manage_file('delete', test_file, None)
-            print(result)
-            assert result['status'] == 200
-            assert result['action'] == 'delete'
-            assert result['path'] == test_file
-            # Make sure that the test file was deleted
-            assert manage_file('get', test_file, None)['status'] == 404
-        else:
-            # If the test file does not exist, log that it did not exist
-            print("Test file did not exist: " + test_file)
-
-    
-        
-
-# Create test files
-def setup_test_files():
-    ##
-    ## GET TEST FILES
-    ##
-    # Cleanup previous test files
-    cleanup_test_files()
-    # Create local test txt file
-    with open(test_txt_get, 'w') as f:
-        f.write(test_txt_content)
-    # Make sure that the test_txt_get file was created
-    assert os.path.exists(test_txt_get)
-    # Create local test binary file
-    with open(test_bin_get, 'wb') as f:
-        f.write(test_bin_content)
-    # Make sure that the test_bin_get file was created
-    assert os.path.exists(test_bin_get)
-    # Upload test_txt_get file to test_txt_get location
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_txt_get, s3_bucket_name, test_txt_get)
-    # Make sure that the test_txt_get file was created
-    compare_s3_local_file(test_txt_get, test_txt_get)
-    # Upload test_bin_get file to test_bin_get_s3 location
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(test_bin_get, s3_bucket_name, test_bin_get)
-    # Make sure that the test_bin_get_s3 file was created
-    compare_s3_local_file(test_bin_get, test_bin_get)
-
-#setup_test_files()
-
-# Test 12 - Test aws credentials
-def test_check_aws_access_key_id():
-    assert AWS_ACCESS_KEY_ID is not None
-
-# Test 13 - Test aws credentials
-def test_check_aws_secret_access_key():
-    assert AWS_SECRET_ACCESS_KEY is not None
-
-
-# Test 7 - post s3 text file
-def test_post_s3_txt_file():
-    result = manage_file(action='post', path="s3://"+s3_bucket_name+"/"+test_txt_post, content=test_txt_content, debug=True)
-    print(result)
-    assert result['status'] == 200
-    assert result['action'] == 'post'
-    assert result['path'] == "s3://"+s3_bucket_name+"/"+test_txt_post
-    assert result['binary'] is False
-    # Additional check: Read the file from s3 to make sure content was written
-    # correctly
-    validate = manage_file('get', "s3://"+s3_bucket_name+"/"+test_txt_post, None, debug=True)
-    print(validate)
-    assert validate['status'] == 200
-    assert validate['action'] == 'get'
-    assert validate['content'].decode() == test_txt_content
-    assert validate['path'] == "s3://"+s3_bucket_name+"/"+test_txt_post
-
-#test_post_s3_txt_file()
-
-# Basic manage_file test - write test_txt_content text to tests/text.txt
-def test_post_local_txt_file():
-    result = manage_file(action='post', path=test_txt_post, content=test_txt_content, debug=True)
-    print(result)
-
-#test_post_local_txt_file()
-
-# Basic manage_file test - write test_txt_content text to s3
-# fsg-gobbler/tests/test_post_txt_file.txt
-def test_post_s3_txt_file():
-    print(f"Posting to: path=s3://fsg-gobbler/{test_txt_post}")
-    result = manage_file(action='post', path="s3://fsg-gobbler/"+test_txt_post, content=test_txt_content, debug=True)
-    
-#    print(result)
-
-
-
-
-
-
 def test_large_upload_progress():
     # Set test files name
-    file_name = './large_file3'
+    file_name = './large_file'
     
     # Generate a 100MB file using dd command
-    subprocess.run(['dd', 'if=/dev/zero', 'of='+file_name, 'bs=1b', 'count=1'])
+    subprocess.run(['dd', 'if=/dev/zero', 'of='+file_name, 'bs=1M', 'count=100'])
 
     # Get md5 hash of the generated file
     with open(file_name, 'rb') as f:
@@ -172,19 +43,15 @@ def test_large_upload_progress():
         
         # Get the md5 hash of the file content
         md5_hash = hashlib.md5(file_content).digest()
-        print(f"md (binary):                    {md5_hash}")
 
         # Encode the md5 hash of the file content to base64 to send to S3
         contents_md5 = base64.b64encode(md5_hash).decode('utf-8')
-        print(f"contents_md5 (base64):          {contents_md5}")
         
         # Convert md5_hash to a hex string for metadata and logging
         md5_hash_hex = md5_hash.hex()
-        print(f"The MD5 Hash of this file is:   {md5_hash_hex}")
 
         # Get & announce the file size in bytes
         file_size = len(file_content)
-        print(f"The file size is: {file_size} bytes")
 
         # Get the file size in either bytes, kilobytes, megabytes or gigabytes depending
         # on how large it is, showing up to 6 decimal places, returning a
@@ -214,54 +81,13 @@ def test_large_upload_progress():
         content=file_content,
         md5=md5_hash_hex,
         metadata=metadata,
-        debug=True
-    )
-
-    print(f"Upload result: {result}")
-
-    # Delete the generated file
-    #os.remove('./large_file')
-
-    
-
-# test_large_upload_progress()
-
-
-def test_text_upload_progress():
-    # Content to upload
-    content="Hello World!"
-    
-    # Get the md5 hash of the content
-    md5_hash = hashlib.md5(content.encode('utf-8')).digest()
-
-    # Convert md5_hash to a hex string for metadata and logging
-    md5_hash_hex = md5_hash.hex()
-
-    # Create metadata
-    metadata = {
-        "md5": md5_hash_hex,
-        "filesize": file_size
-    }
-
-    # Upload the file with progress callback and dump the full response from
-    # klingon-file-manager to console
-    result = manage_file(
-        action='post',
-        path="s3://fsg-gobbler/tests/test-txt-file.txt",
-        content=content,
-        md5=md5_hash_hex,
-        metadata=metadata,
+        progress=true,
         debug=False
     )
 
-    # Delete the generated file
-    #os.remove('./large_file')
+    # Remove the test file
+    os.remove(file_name)
 
-    # Sleep for 5 seconds to allow the file to sync within s3's systems
-
-    
-
+    print(f"Upload result: {result}")
 
 test_large_upload_progress()
-# test_text_upload_progress()
-
