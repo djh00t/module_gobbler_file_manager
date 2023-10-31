c5e3b49d79f1273d3869ea3137134ae679288d00
David Hooton <david@hooton.org>
2023-11-01 00:55:03 +1100

    aider: Add detailed pdoc style documentation to the `__init__.py` file in the `klingon_file_manager` directory.
    # Aider chat conversation:
    
    USER: Add detailed module-wide documentation to this file showing the overall structure, features and functions of the library
    
    ASSISTANT: Sure, I can help with that. Here's a proposed update to the `__init__.py` file in the `klingon_file_manager` directory. This update includes a more detailed docstring that describes the overall structure, features, and functions of the library.
    
    ```
    ^C KeyboardInterrupt
    
    USER: Add detailed module-wide pdoc style documentation to this file showing the overall structure, features and functions of the library
    
    ASSISTANT: Sure, I can help with that. Here's a proposed update to the `__init__.py` file in the `klingon_file_manager` directory. This update includes a more detailed pdoc-style docstring that describes the overall structure, features, and functions of the library.
    
    ```python
    klingon_file_manager/__init__.py
    <<<<<<< SEARCH
    # __init__.py
    """
    File Management Service for Local and AWS S3 Storage.
    """
    
    from .main import manage_file
    from .delete import delete_file
    from .get import get_file
    from .post import post_file
    from .utils import get_mime_type, get_aws_credentials, is_binary_file
    =======
    # __init__.py
    """
    klingon_file_manager
    --------------------
    
    A File Management Service for Local and AWS S3 Storage.
    
    This library provides a set of functions to manage files both locally and on AWS S3.
    
    Functions:
    - manage_file: Main function to manage files. It can handle operations like upload, download, delete, etc.
    - delete_file: Function to delete a file from local storage or AWS S3.
    - get_file: Function to get a file from local storage or AWS S3.
    - post_file: Function to post a file to local storage or AWS S3.
    - get_mime_type: Utility function to get the MIME type of a file.
    - get_aws_credentials: Utility function to get AWS credentials from environment variables.
    - is_binary_file: Utility function to check if a file is binary.
    
    """
    
    from .main import manage_file
    from .delete import delete_file
    from .get import get_file
    from .post import post_file
    from .utils import get_mime_type, get_aws_credentials, is_binary_file
    >>>>>>> REPLACE
    ```
    
    This docstring provides a brief overview of the library and lists the functions it provides. It also gives a short description of what each function does.

--- klingon_file_manager/__init__.py
+++ klingon_file_manager/__init__.py
@@ -1,6 +1,21 @@
 # __init__.py
 """
-File Management Service for Local and AWS S3 Storage.
+klingon_file_manager
+--------------------
+
+A File Management Service for Local and AWS S3 Storage.
+
+This library provides a set of functions to manage files both locally and on AWS S3. 
+
+Functions:
+- manage_file: Main function to manage files. It can handle operations like upload, download, delete, etc.
+- delete_file: Function to delete a file from local storage or AWS S3.
+- get_file: Function to get a file from local storage or AWS S3.
+- post_file: Function to post a file to local storage or AWS S3.
+- get_mime_type: Utility function to get the MIME type of a file.
+- get_aws_credentials: Utility function to get AWS credentials from environment variables.
+- is_binary_file: Utility function to check if a file is binary.
+
 """
 
 from .main import manage_file
