1f3eb37041c9497012b4d2029f5a725c9e366070
David Hooton <david@hooton.org>
2023-11-03 12:22:41 +1100

    aider: Refactor test_environment_aws_credentials to use monkeypatch and assert the exception message.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- tests/test_test_test.py
+++ tests/test_test_test.py
@@ -4,51 +4,16 @@ from klingon_file_manager.utils import get_aws_credentials
 
 def mock_getenv(key, default=None):
     return {
-        'AWS_ACCESS_KEY_ID': 'AKIAIOSFODNN7EXAMPLE',
         'AWS_SECRET_ACCESS_KEY': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
     }.get(key, default)
 
-def get_mock_iam_client():
-    mock_iam_client = MagicMock()
-    mock_iam_client.get_user.return_value = {
-        'User': {
-            'UserId': 'mock-user-id',
-            'Arn': 'arn:aws:iam::123456789012:user/mock-user'
-        }
-    }
-    return mock_iam_client
+# ... existing helper functions ...
 
-def get_mock_s3_client():
-    mock_s3_client = MagicMock()
-    mock_s3_client.list_buckets.return_value = {
-        'Buckets': []
-    }
-    return mock_s3_client
+def test_environment_aws_credentials(aws_credentials_fixture, monkeypatch):
+    monkeypatch.setattr('os.getenv', mock_getenv)
 
-def get_mocked_session():
-    mock_iam_client = get_mock_iam_client()
-    mock_s3_client = get_mock_s3_client()
+    with patch('klingon_file_manager.utils.Session', return_value=get_mocked_session()):
+        with pytest.raises(Exception) as e:
+            get_aws_credentials()
 
-    mock_session = MagicMock()
-    mock_session.client.side_effect = lambda service_name, **kwargs: mock_iam_client if service_name == 'iam' else mock_s3_client
-
-    return mock_session
-
-@pytest.fixture
-def aws_credentials_fixture():
-    return {
-        'credentials': {
-            'AWS_ACCESS_KEY_ID': 'AKIAIOSFODNN7EXAMPLE',
-            'AWS_SECRET_ACCESS_KEY': 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
-        }
-    }
-
-def test_environment_aws_credentials(aws_credentials_fixture):
-    with patch('os.getenv', side_effect=mock_getenv):
-        with patch('klingon_file_manager.utils.Session', return_value=get_mocked_session()):
-            response = get_aws_credentials()
-            print(f"Access Key in the test response: {response['credentials']['AWS_ACCESS_KEY_ID']}")
-    assert 'credentials' in response
-    assert response['credentials']['AWS_ACCESS_KEY_ID'] == 'AKIAIOSFODNN7EXAMPLE'
-
-### 
+    assert str(e.value) == 'Missing AWS_ACCESS_KEY_ID in environment variables'
